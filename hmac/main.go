6c5658599817285168877b526799ede8326af2d3
Doug Richardson <dougie.richardson@gmail.com>
2015-12-23 20:53:40 -0800

    wikipedia example comparison

--- hmac/main.go
+++ hmac/main.go
@@ -22,6 +22,15 @@ func main() {
 	fmt.Println("checkmac (bad key)", CheckMAC([]byte(message), sum, key[:len(key)-1]))
 	fmt.Println("checkmac (+1)", CheckMAC([]byte(message+"1"), sum, key))
 	fmt.Println("checkmac (-1)", CheckMAC([]byte(message[:len(message)-1]), sum, key))
+
+	h := hmac.New(sha256.New, []byte("key"))
+	h.Write([]byte("The quick brown fox jumps over the lazy dog"))
+	fmt.Println("wikipedia check: f7bc83f430538424b13298e6aa6fb143ef4d59a14946175997479dbc2d1a3cd8=?", hex.EncodeToString(h.Sum(nil)))
+
+	h2 := hmac.New(sha256.New, []byte(""))
+	h2.Write([]byte(""))
+	fmt.Println("wikipedia check: b613679a0814d9ec772f95d778c35fc5ff1697c493715653c6c712144292c5ad=?", hex.EncodeToString(h2.Sum(nil)))
+
 }
 
 // CheckMAC reports whether messageMAC is a valid HMAC tag for message.
