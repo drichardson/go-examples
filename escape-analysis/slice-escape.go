5df0ed6210c4adc17ab75941e89b4357b7ca881e
Doug Richardson <dougie.richardson@gmail.com>
2016-01-22 12:51:43 -0800

    annotate slice escape analysis

--- escape-analysis/slice-escape.go
+++ escape-analysis/slice-escape.go
@@ -2,16 +2,32 @@ package main
 
 func main() {
 	xyz := [3]int{1, 2, 3}
+	// main xyz does not escape
 	xyzSlice := xyz[:]
 	f(xyzSlice)
+	h(xyzSlice)
 }
 
 func g(x int) int {
 	return x
 }
 
+// f ints does not escape
 func f(ints []int) {
 	for _, i := range ints {
 		g(i)
 	}
+}
+
+// leaking param: ints to result result level=0
+func h(ints []int) (result []int) {
+	result = ints
+	return result
+}
+
+// i ints does not escape
+func i(ints []int) (result []int) {
+	// []int literal escapes to heap
+	result = []int{4, 5, 6}
+	return result
 }
