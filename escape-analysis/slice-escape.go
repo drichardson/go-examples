40cd926bbfee049eddf01c09bd05196adcea66a8
Doug Richardson <dougie.richardson@gmail.com>
2016-01-22 18:58:25 -0800

    more escape tests

--- escape-analysis/slice-escape.go
+++ escape-analysis/slice-escape.go
@@ -2,7 +2,6 @@ package main
 
 func main() {
 	xyz := [3]int{1, 2, 3}
-	// main xyz does not escape
 	xyzSlice := xyz[:]
 	f(xyzSlice)
 	h(xyzSlice)
@@ -12,22 +11,36 @@ func g(x int) int {
 	return x
 }
 
-// f ints does not escape
 func f(ints []int) {
 	for _, i := range ints {
 		g(i)
 	}
 }
 
-// leaking param: ints to result result level=0
 func h(ints []int) (result []int) {
 	result = ints
 	return result
 }
 
-// i ints does not escape
 func i(ints []int) (result []int) {
-	// []int literal escapes to heap
 	result = []int{4, 5, 6}
 	return result
+}
+
+func j() (result []int) {
+	var x [3]int = [3]int{3, 2, 1}
+	s := x[:0]
+	s[0] = 4
+	return
+}
+
+func k() (result [3]int) {
+	result = [3]int{3, 2, 1}
+	return
+}
+
+func l() (result *[3]int) {
+	x := &[3]int{3, 2, 1}
+	result = x
+	return
 }
