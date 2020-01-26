527c645059df9f279c533625396213a20c31f04d
Doug Richardson <doug@rekt.email>
2020-01-25 21:51:49 -0800

    dtls kind of working... handles first connection and then stops working

--- dtls/server/server.go
+++ dtls/server/server.go
@@ -3,18 +3,67 @@ package main
 import (
 	"flag"
 	"fmt"
-	//"github.com/pion/dtls"
+	"github.com/pion/dtls"
 	"net"
 )
 
-var encrypt *bool = flag.Bool("encrypt", false, "Use DTLS to encrypt connection.")
-var address *string = flag.String("address", ":5959", "Address to listen on.")
+var encrypt = flag.Bool("encrypt", false, "Use DTLS to encrypt connection.")
+var address = flag.String("address", ":5959", "Address to listen on.")
 
 func main() {
 	fmt.Println("server")
 
 	flag.Parse()
 
+	if *encrypt {
+		serveDTLS()
+	} else {
+		servePlaintext()
+	}
+}
+
+func serveDTLS() {
+	cert, privateKey, err := dtls.GenerateSelfSigned()
+
+	config := &dtls.Config{
+		Certificate:        cert,
+		PrivateKey:         privateKey,
+		InsecureSkipVerify: true,
+	}
+
+	laddr, err := net.ResolveUDPAddr("udp", *address)
+	if err != nil {
+		panic(err)
+	}
+	listener, err := dtls.Listen("udp", laddr, config)
+
+	if err != nil {
+		panic(err)
+	}
+
+	buffer := make([]byte, 1024)
+
+	for {
+		fmt.Println("accept")
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("read")
+		n, err := conn.Read(buffer)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Read %d bytes from %v. Data: %s\n", n, conn.RemoteAddr(), string(buffer[0:n]))
+
+		fmt.Println("close")
+		conn.Close()
+	}
+}
+
+func servePlaintext() {
 	pc, err := net.ListenPacket("udp", *address)
 	if err != nil {
 		panic(err)
